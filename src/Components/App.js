7a58b616f3a828906e036240b2a18a5f598b137f
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-04 13:08:28 +0600

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return <>
+   <div className="pen top_pen"></div>
+   <div className="pen">
+   <iframe
+   title="output" 
+   sandbox="allow-scripts"
+   />
+
+   </div>
+
+  </>
+}
+
+export default App;
