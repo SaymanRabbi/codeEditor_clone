9d56e7a8ba0c5bb5bb7a0793ae1da0815af5481a
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-04 13:13:46 +0600

    add base setup

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,20 @@
+import Editor from "./Editor";
+
 function App() {
   return <>
-   <div className="pen top_pen"></div>
+   <div className="pen top_pen">
+<Editor/>
+<Editor/>
+<Editor/>
+
+   </div>
    <div className="pen">
    <iframe
    title="output" 
    sandbox="allow-scripts"
+   frameBorder="0"
+    width="100%"
+    height="500px"
    />
 
    </div>
---
+++ src/Components/Editor.js
@@ -0,0 +1,15 @@
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/theme/material.css';
+const Editor = () => {
+    return (
+        <div>
+            Editor
+        </div>
+    );
+};
+
+export default Editor;
